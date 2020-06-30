3e41f76e28073ec558ab338fd241bb604b8f668c
Brian Beck <bbeck@ibtapps.com>
2020-06-30 17:51:54 -0500

    Add project files.

---
+++ SwitchControlToggleDialog/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SwitchControlToggleDialog.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+		private readonly IDialogService _dialogService;
+
+		protected IDialogService DialogService { get; private set; }
+
+		public MainPageViewModel(INavigationService navigationService, IDialogService dialogService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+			_dialogService = dialogService;
+
+			this.AllowUseToggledCommand = new DelegateCommand(this.AllowUseToggled);
+		}
+
+		public DelegateCommand AllowUseToggledCommand { get; set; }
+
+		private void AllowUseToggled()
+		{
+			var message = "This is a test message for the test dialog";
+			DialogParameters dialogParameters = new DialogParameters
+			{
+				{ "Title", "Switch toggled Dialog" },
+				{ "Message", message },
+			};
+
+			//await this._dialogService.ShowDialogAsync("SwitchToggleDialog", dialogParameters);
+			this._dialogService.ShowDialog("SwitchToggleDialog", dialogParameters);
+			//this.ProcessDialogNavigationRequest(dialogResult);
+		}
+	}
+}
---
+++ SwitchControlToggleDialog/ViewModels/SwitchToggleDialogViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Prism.Commands;
+using Prism.Navigation;
+using Prism.Services.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwitchControlToggleDialog.ViewModels
+{
+    
+
+    public class SwitchToggleDialogViewModel : ViewModelBase, IDialogAware
+    {
+        private IDialogService _dialogService;
+
+        private string _message;
+
+        public event Action<IDialogParameters> RequestClose;
+
+        protected IDialogService DialogService { get; private set; }
+
+        public SwitchToggleDialogViewModel(INavigationService navigationService, IDialogService dialogService)
+            : base(navigationService)
+        {
+            Title = "Testing... Dialog works with button";
+            _dialogService = dialogService;
+            CloseCommand = new DelegateCommand(()=>RequestClose(null));
+        }
+
+        public string Message
+        {
+            get => _message;
+            set => SetProperty(ref _message, value);
+        }
+
+        public DelegateCommand CloseCommand { get; }
+
+        private void ExecuteCancel()
+        {
+
+        }
+
+        public bool CanCloseDialog() => true;
+        
+        public void OnDialogClosed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+            Message = parameters.GetValue<string>("message");
+        }
+    }
+}
